98b770f1ca196a162490223563323b5cb2fa24f2
martahil <marta.hil.1988@gmail.com>
2023-11-02 20:34:55 -0300

    Aula 12 Exercício 15

--- Aula_12ex/ex015/script.js
+++ Aula_12ex/ex015/script.js
@@ -7,6 +7,50 @@ function verificar(){
     if(fAno.value.length == 0 || Number(fAno.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
     } else {
-       // var fsex = 
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fAno.value)
+       //res.innerHTML = `Idade calculada: ${idade}`
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       img.style.padding = '20px'
+       if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade <10){
+                //criança
+                img.setAttribute('src', 'menino.png')
+            } else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'adolescento.png')
+            } else if(idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso.png')
+            }
+       }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade <10){
+                //criança
+                
+                img.setAttribute('src', 'menina.png')
+            } else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'adolescenta.png')
+            } else if(idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa.png')
+            }
+       }
+       res.style.textAlign = 'center'
+       
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       
+       res.appendChild(img)
+       //res.style.padding = '100px'
     }
 }
