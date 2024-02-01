613dc11bf1ceeaf5f619e3c9b6ab346f0a9d5688
Marta Hilgert <marta.hil.1988@gmail.com>
2024-02-01 16:46:52 -0300

    Ajustes gerais, borda na foto de perfil, borda ao redor dos posts, botão de like e limpeza de código

---
+++ Arquivos base/likeButton.js
@@ -0,0 +1,27 @@
+var likeContador1 = 32;
+function buttonClicked1() {
+    document.getElementById('likeNum1').value = ++likeContador1;
+}
+
+var likeContador2 = 16;
+function buttonClicked2() {
+    document.getElementById('likeNum2').value = ++likeContador2;
+}
+
+var likeContador3 = 29;
+function buttonClicked3() {
+    document.getElementById('likeNum3').value = ++likeContador3;
+}
+
+var likeContador4 = 16;
+function buttonClicked4() {
+    document.getElementById('likeNum4').value = ++likeContador4;
+}
+
+var likeContador5 = 18;
+function buttonClicked5() {
+    document.getElementById('likeNum5').value = ++likeContador5;
+}
+
+
+
